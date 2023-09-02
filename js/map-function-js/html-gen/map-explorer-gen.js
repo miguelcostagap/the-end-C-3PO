9bb0423f625f791b255f9dff2d3e58f0440240ad
Miguel Costa <miguelcostagap@gmail.com>
2023-09-02 18:38:09 +0100

    map-features-functional

---
+++ js/map-function-js/html-gen/map-explorer-gen.js
@@ -0,0 +1,37 @@
+export function mapExplorerGen() {
+    let mapExplorerGenHtml = `
+    <div class="mapExplorerCoords">
+    <div class="grid-background"> <img src="./Assets/testing_temporary/zone-all.png" alt="" class="overlay-image">
+
+    </div>
+
+    <div class="div1">
+        <svg width="100%" height="100%" viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <polygon class="hoverable" points="0,0 50,35 30,45 8,18 20,5"
+             />
+        </svg>
+    </div>
+
+    <div class="div2" id="region1">
+        <svg width="100%" height="auto" viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <polygon class="hoverable" points="1,0.75 2,1.05 1.2,1.35 0.32,0.54 0.8,0.15"
+            onclick="alert('I am click functionality!');"  />
+        </svg>
+    </div>
+
+    <div class="div3"> </div>
+    <div class="div4"> </div>
+    <div class="div5"> </div>
+    <div class="div6"> </div>
+    <div class="div7"> </div>
+    <div class="div8"> </div>
+    <div class="div9"> </div>
+</div>
+    `;
+    return mapExplorerGenHtml
+}
+
+
+
+
+
